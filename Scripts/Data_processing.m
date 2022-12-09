7921a96b53a7cc4f4ad9d740fb3c4393c202c3c3
Lorenzo Leoni <l.leoni2@studenti.unibg.it>
2022-12-09 08:51:00 +0100

    Modified data structures in order to distinguish daily data from hourly data (for estimation of FDA models)

--- Scripts/Data_processing.m
+++ Scripts/Data_processing.m
@@ -16,78 +16,107 @@ months = ["January" "February" "March" "April" "May" "June" "July" "August"...
     "September" "October" "November" "December"];
 
 DS = readtable(bike_path + "\" + months(1, 1) + "2020.csv");
-calendar = datetime(2020, 1, (1:366));
+daily_calendar = datetime(2020, 1, (1:366));
 id_stations = sort(unique(DS.startStationId));
+hourly_calendar = repmat(datetime('now'), 24, 366);
+for i=1:366
+    hourly_calendar(:, i) = datetime(2020, 1, i, (0:23), 0, 0);
+end
+hourly_calendar = hourly_calendar(:)';
 
-counters_table = array2table(NaN(length(id_stations), length(calendar)+1));
-counters_table.Properties.VariableNames = ["idStation" string(calendar)];
-counters_table.idStation = id_stations;
+daily_counters_table = array2table(NaN(length(id_stations), length(daily_calendar)+1));
+daily_counters_table.Properties.VariableNames = ["idStation" string(daily_calendar)];
+daily_counters_table.idStation = id_stations;
+hourly_counters_table = array2table(NaN(length(id_stations), length(hourly_calendar)+1));
+hourly_counters_table.Properties.VariableNames = ["idStation" string(hourly_calendar)];
+hourly_counters_table.idStation = id_stations;
 
-age_table = array2table(NaN(length(id_stations), length(calendar)+1));
-age_table.Properties.VariableNames = ["idStation" string(calendar)];
-age_table.idStation = id_stations;
+daily_age_table = array2table(NaN(length(id_stations), length(daily_calendar)+1));
+daily_age_table.Properties.VariableNames = ["idStation" string(daily_calendar)];
+daily_age_table.idStation = id_stations;
+hourly_age_table = array2table(NaN(length(id_stations), length(hourly_calendar)+1));
+hourly_age_table.Properties.VariableNames = ["idStation" string(hourly_calendar)];
+hourly_age_table.idStation = id_stations;
 
-duration_table = array2table(NaN(length(id_stations), length(calendar)+1));
-duration_table.Properties.VariableNames = ["idStation" string(calendar)];
-duration_table.idStation = id_stations;
+daily_duration_table = array2table(NaN(length(id_stations), length(daily_calendar)+1));
+daily_duration_table.Properties.VariableNames = ["idStation" string(daily_calendar)];
+daily_duration_table.idStation = id_stations;
+hourly_duration_table = array2table(NaN(length(id_stations), length(hourly_calendar)+1));
+hourly_duration_table.Properties.VariableNames = ["idStation" string(hourly_calendar)];
+hourly_duration_table.idStation = id_stations;
 
-male_table = array2table(NaN(length(id_stations), length(calendar)+1));
-male_table.Properties.VariableNames = ["idStation" string(calendar)];
-male_table.idStation = id_stations;
+daily_male_table = array2table(NaN(length(id_stations), length(daily_calendar)+1));
+daily_male_table.Properties.VariableNames = ["idStation" string(daily_calendar)];
+daily_male_table.idStation = id_stations;
+hourly_male_table = array2table(NaN(length(id_stations), length(hourly_calendar)+1));
+hourly_male_table.Properties.VariableNames = ["idStation" string(hourly_calendar)];
+hourly_male_table.idStation = id_stations;
 
-female_table = array2table(NaN(length(id_stations), length(calendar)+1));
-female_table.Properties.VariableNames = ["idStation" string(calendar)];
-female_table.idStation = id_stations;
+daily_female_table = array2table(NaN(length(id_stations), length(daily_calendar)+1));
+daily_female_table.Properties.VariableNames = ["idStation" string(daily_calendar)];
+daily_female_table.idStation = id_stations;
+hourly_female_table = array2table(NaN(length(id_stations), length(hourly_calendar)+1));
+hourly_female_table.Properties.VariableNames = ["idStation" string(hourly_calendar)];
+hourly_female_table.idStation = id_stations;
 
-unknown_table = array2table(NaN(length(id_stations), length(calendar)+1));
-unknown_table.Properties.VariableNames = ["idStation" string(calendar)];
-unknown_table.idStation = id_stations;
+daily_unknown_table = array2table(NaN(length(id_stations), length(daily_calendar)+1));
+daily_unknown_table.Properties.VariableNames = ["idStation" string(daily_calendar)];
+daily_unknown_table.idStation = id_stations;
+hourly_unknown_table = array2table(NaN(length(id_stations), length(hourly_calendar)+1));
+hourly_unknown_table.Properties.VariableNames = ["idStation" string(hourly_calendar)];
+hourly_unknown_table.idStation = id_stations;
 
-subscriber_table = array2table(NaN(length(id_stations), length(calendar)+1));
-subscriber_table.Properties.VariableNames = ["idStation" string(calendar)];
-subscriber_table.idStation = id_stations;
+daily_subscriber_table = array2table(NaN(length(id_stations), length(daily_calendar)+1));
+daily_subscriber_table.Properties.VariableNames = ["idStation" string(daily_calendar)];
+daily_subscriber_table.idStation = id_stations;
+hourly_subscriber_table = array2table(NaN(length(id_stations), length(hourly_calendar)+1));
+hourly_subscriber_table.Properties.VariableNames = ["idStation" string(hourly_calendar)];
+hourly_subscriber_table.idStation = id_stations;
 
-customer_table = array2table(NaN(length(id_stations), length(calendar)+1));
-customer_table.Properties.VariableNames = ["idStation" string(calendar)];
-customer_table.idStation = id_stations;
+daily_customer_table = array2table(NaN(length(id_stations), length(daily_calendar)+1));
+daily_customer_table.Properties.VariableNames = ["idStation" string(daily_calendar)];
+daily_customer_table.idStation = id_stations;
+hourly_customer_table = array2table(NaN(length(id_stations), length(hourly_calendar)+1));
+hourly_customer_table.Properties.VariableNames = ["idStation" string(hourly_calendar)];
+hourly_customer_table.idStation = id_stations;
 
 day_counter = 1;
 for i=1:length(months)
     disp(months(1, i) + " done.")
     Bike_data = readtable(bike_path + "\" + months(1, i) + "2020.csv");
-    for k=1:sum(month(calendar, 'name') == months(1, i))
+    for k=1:sum(month(daily_calendar, 'name') == months(1, i))
         for j=1:length(id_stations)
             selector = Bike_data.startStationId == id_stations(j, 1)...
                 & Bike_data.starttime.Day == k;
             temp = Bike_data(selector, :);
             % counting of the number of starts per station
             count = size(temp, 1);
-            counters_table(counters_table.idStation==id_stations(j, 1),...
+            daily_counters_table(daily_counters_table.idStation==id_stations(j, 1),...
                 day_counter + 1) = {count};
             % calculation of the average users' age per station
             avg_age = mean(2020*ones(size(temp, 1), 1) - temp{:, "birthYear"});
-            age_table(counters_table.idStation==id_stations(j, 1),...
+            daily_age_table(daily_counters_table.idStation==id_stations(j, 1),...
                 day_counter + 1) = {avg_age};
             % calculation of the average trip duration per station
             avg_duration = mean(temp{:, "tripduration"});
-            duration_table(counters_table.idStation==id_stations(j, 1),...
+            daily_duration_table(daily_counters_table.idStation==id_stations(j, 1),...
                 day_counter + 1) = {avg_duration};
             % counting of the male/female/unknown users per station
             male_count = size(temp(temp.gender==1, :), 1);
-            male_table(counters_table.idStation==id_stations(j, 1),...
+            daily_male_table(daily_counters_table.idStation==id_stations(j, 1),...
                 day_counter + 1) = {male_count};
             female_count = size(temp(temp.gender==2, :), 1);
-            female_table(counters_table.idStation==id_stations(j, 1),...
+            daily_female_table(daily_counters_table.idStation==id_stations(j, 1),...
                 day_counter + 1) = {female_count};
             unknown_count = size(temp(temp.gender==0, :), 1);
-            unknown_table(counters_table.idStation==id_stations(j, 1),...
+            daily_unknown_table(daily_counters_table.idStation==id_stations(j, 1),...
                 day_counter + 1) = {unknown_count};
             % counting of the subscriber/customer users per station
             subscriber_count = size(temp(temp.usertype=="Subscriber", :), 1);
-            subscriber_table(counters_table.idStation==id_stations(j, 1),...
+            daily_subscriber_table(daily_counters_table.idStation==id_stations(j, 1),...
                 day_counter + 1) = {subscriber_count};
             customer_count = size(temp(temp.usertype=="Customer", :), 1);
-            customer_table(counters_table.idStation==id_stations(j, 1),...
+            daily_customer_table(daily_counters_table.idStation==id_stations(j, 1),...
                 day_counter + 1) = {customer_count};
         end
         day_counter = day_counter + 1;
@@ -114,13 +143,15 @@ end
 % - Thursday, November 26 – Thanksgiving Day
 % - Friday, December 25 – Christmas Day
 
-calendar = convertTo(datetime(2020, 1, (1:366)), "datenum");
+daily_calendar = convertTo(daily_calendar, "datenum");
+hourly_calendar = convertTo(hourly_calendar, "datenum");
+
 sundays = convertTo(datetime(2020, 1, 5), "datenum"):7:convertTo(datetime(2020, 12, 27), "datenum");
 holidays = convertTo([datetime(2020, 1, 1) datetime(2020, 1, 20) ...
     datetime(2020, 2, 17) datetime(2020, 5, 25) datetime(2020, 7, 3) ...
     datetime(2020, 9, 7) datetime(2020, 10, 12) datetime(2020, 11, 11) ...
     datetime(2020, 11, 26) datetime(2020, 12, 25)], "datenum");
-non_working_days = ismember(calendar, [sundays holidays]);
+non_working_days = ismember(daily_calendar, [sundays holidays]);
 
 % Lockdown dates due to COVID-19 pandemic in New York City:
 % - March 22, 2020: NYS on Pause Program begins, all non-essential workers
@@ -130,23 +161,18 @@ non_working_days = ismember(calendar, [sundays holidays]);
 
 lockdown = convertTo(datetime(2020, 3, 22),...
     "datenum"):1:convertTo(datetime(2020, 5, 15), "datenum");
-lockdown_days = ismember(calendar, lockdown);
-
-clearvars -except counters_table duration_table age_table male_table...
-    female_table unknown_table subscriber_table customer_table Bike_data...
-    calendar num_stations id_stations lat_stations lon_stations non_working_days...
-    lockdown_days
+lockdown_days = ismember(daily_calendar, lockdown);
 
 % Data formatting and export
 
-bike_sharing_data.data{1} = counters_table{:,2:end};
-bike_sharing_data.data{2} = duration_table{:,2:end};
-bike_sharing_data.data{3} = age_table{:,2:end};
-bike_sharing_data.data{4} = male_table{:,2:end};
-bike_sharing_data.data{5} = female_table{:,2:end};
-bike_sharing_data.data{6} = unknown_table{:,2:end};
-bike_sharing_data.data{7} = subscriber_table{:,2:end};
-bike_sharing_data.data{8} = customer_table{:,2:end};
+bike_sharing_data.daily_data{1} = daily_counters_table{:,2:end};
+bike_sharing_data.daily_data{2} = daily_duration_table{:,2:end};
+bike_sharing_data.daily_data{3} = daily_age_table{:,2:end};
+bike_sharing_data.daily_data{4} = daily_male_table{:,2:end};
+bike_sharing_data.daily_data{5} = daily_female_table{:,2:end};
+bike_sharing_data.daily_data{6} = daily_unknown_table{:,2:end};
+bike_sharing_data.daily_data{7} = daily_subscriber_table{:,2:end};
+bike_sharing_data.daily_data{8} = daily_customer_table{:,2:end};
 bike_sharing_data.var_names{1} = 'access counters';
 bike_sharing_data.var_names{2} = 'avg trip duration';
 bike_sharing_data.var_names{3} = 'avg clients age';
@@ -163,10 +189,12 @@ bike_sharing_data.unit_of_measure{5} = 'dimensionless';
 bike_sharing_data.unit_of_measure{6} = 'dimensionless';
 bike_sharing_data.unit_of_measure{7} = 'dimensionless';
 bike_sharing_data.unit_of_measure{8} = 'dimensionless';
-bike_sharing_data.date_time = calendar;
+bike_sharing_data.daily_calendar = daily_calendar;
+bike_sharing_data.hourly_calendar = hourly_calendar;
 bike_sharing_data.non_working_days = non_working_days;
 bike_sharing_data.lockdown_days = lockdown_days;
-bike_sharing_data.temporal_granularity  = 'day';
+bike_sharing_data.temporal_granularity{1}  = 'day';
+bike_sharing_data.temporal_granularity{2}  = 'hour';
 bike_sharing_data.measure_type = 'observed';
 bike_sharing_data.data_type = 'point';
 bike_sharing_data.data_source = 'https://www.kaggle.com/datasets/vineethakkinapalli/citibike-bike-sharingnewyork-cityjan-to-apr-2021';
@@ -191,21 +219,29 @@ clearvars -except bike_sharing_data non_working_days lockdown_days
 
 %  Information extraction
 
-meteo_path = "C:\Users\loren\OneDrive - unibg.it\University\S4HDD (Statistics for High Dimensional Data)\Project\Data\NYC_meteo_daily_data.csv";
-Meteo_DS = readtable(meteo_path);
-calendar = convertTo(datetime(2020, 1, (1:366)), "datenum");
+daily_meteo_path = "C:\Users\loren\OneDrive - unibg.it\University\S4HDD (Statistics for High Dimensional Data)\Project\Data\NYC_meteo_daily_data.csv";
+hourly_meteo_path = "C:\Users\loren\OneDrive - unibg.it\University\S4HDD (Statistics for High Dimensional Data)\Project\Data\NYC_meteo_hourly_data.csv";
+Daily_meteo_DS = readtable(daily_meteo_path);
+Hourly_meteo_DS = readtable(hourly_meteo_path);
+Hourly_meteo_DS = Hourly_meteo_DS(1:(24*366), :);
+daily_calendar = convertTo(datetime(2020, 1, (1:366)), "datenum");
+hourly_calendar = repmat(datetime('now'), 24, 366);
+for i=1:366
+    hourly_calendar(:, i) = datetime(2020, 1, i, (0:23), 0, 0);
+end
+hourly_calendar = convertTo(hourly_calendar(:)', "datenum");
 
 % Data formatting and export
 
-meteo_data.data{1} = Meteo_DS{:, "temp"}';
-meteo_data.data{2} = Meteo_DS{:, "feelslike"}';
-meteo_data.data{3} = Meteo_DS{:, "humidity"}';
-meteo_data.data{4} = Meteo_DS{:, "precip"}';
-meteo_data.data{5} = Meteo_DS{:, "snow"}';
-meteo_data.data{6} = Meteo_DS{:, "windspeed"}';
-meteo_data.data{7} = Meteo_DS{:, "cloudcover"}';
-meteo_data.data{8} = Meteo_DS{:, "visibility"}';
-meteo_data.data{9} = Meteo_DS{:, "uvindex"}';
+meteo_data.daily_data{1} = Daily_meteo_DS{:, "temp"}';
+meteo_data.daily_data{2} = Daily_meteo_DS{:, "feelslike"}';
+meteo_data.daily_data{3} = Daily_meteo_DS{:, "humidity"}';
+meteo_data.daily_data{4} = Daily_meteo_DS{:, "precip"}';
+meteo_data.daily_data{5} = Daily_meteo_DS{:, "snow"}';
+meteo_data.daily_data{6} = Daily_meteo_DS{:, "windspeed"}';
+meteo_data.daily_data{7} = Daily_meteo_DS{:, "cloudcover"}';
+meteo_data.daily_data{8} = Daily_meteo_DS{:, "visibility"}';
+meteo_data.daily_data{9} = Daily_meteo_DS{:, "uvindex"}';
 meteo_data.var_names{1} = 'avg temperature';
 meteo_data.var_names{2} = 'avg feels like temperature';
 meteo_data.var_names{3} = 'humidity';
@@ -224,10 +260,12 @@ meteo_data.unit_of_measure{6} = 'km/h';
 meteo_data.unit_of_measure{7} = '%';
 meteo_data.unit_of_measure{8} = 'km';
 meteo_data.unit_of_measure{9} = 'dimensionless';
-meteo_data.date_time = calendar;
+meteo_data.daily_calendar = daily_calendar;
+meteo_data.hourly_calendar = hourly_calendar;
 meteo_data.non_working_days = non_working_days;
 meteo_data.lockdown_days = lockdown_days;
-meteo_data.temporal_granularity  = 'day';
+meteo_data.temporal_granularity{1}  = 'day';
+meteo_data.temporal_granularity{2}  = 'hour';
 meteo_data.measure_type = 'observed';
 meteo_data.data_type = 'global';
 meteo_data.data_source = 'https://www.visualcrossing.com/weather/weather-data-services/New%20York/us/last15days#';
@@ -245,13 +283,13 @@ clearvars -except bike_sharing_data meteo_data
 
 %% README.md cover creation
 
-avg_service_usage = mean(bike_sharing_data.data{1}, 1);
-calendar = datetime(bike_sharing_data.date_time, "ConvertFrom", "datenum");
+avg_service_usage = mean(bike_sharing_data.daily_data{1}, 1);
+daily_calendar = datetime(bike_sharing_data.daily_calendar, "ConvertFrom", "datenum");
 start_ld = datetime(2020, 3, 22);
 end_ld = datetime(2020, 5, 15);
 
 figure
-plot(calendar, avg_service_usage)
+plot(daily_calendar, avg_service_usage)
 title(['\textbf{Average number of daily bicycle picks-up at stations '...
     'vs daily rainfall}'], 'Interpreter', 'latex')
 v = [81 0; 135 0; 135 60; 81 60];
@@ -267,5 +305,5 @@ ylabel('Average picks-up [picks-up/station]', 'Interpreter', 'latex')
 text(135, 52, '\textbf{Lockdown}', 'VerticalAlignment', 'baseline', 'Rotation',  90,...
     'Interpreter', 'latex', 'Color', 'red')
 yyaxis right
-plot(calendar, meteo_data.data{4})
+plot(daily_calendar, meteo_data.daily_data{4})
 ylabel('Rainfall [mm]', 'Interpreter', 'latex')
