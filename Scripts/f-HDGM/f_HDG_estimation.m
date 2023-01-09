894fffa74835e76050c9818e9b11e1fa4d118538
Lorenzo Leoni <l.leoni2@studenti.unibg.it>
2023-01-09 12:36:06 +0100

    Updated f-HDGM validation plots

--- Scripts/f-HDGM/f_HDG_estimation.m
+++ Scripts/f-HDGM/f_HDG_estimation.m
@@ -13,7 +13,7 @@ load S_val.mat
 
 %% Regressors configuration
 
-flag = 3;
+flag = 2;
 switch flag
     case 1 % entire dataset
         data = hourly_data;
@@ -26,9 +26,11 @@ end
 %% Objects creation
 
 % model type selection
+
 o_modeltype = stem_modeltype('f-HDGM');
 
 % splines information
+
 input_fda.spline_type = 'Fourier';
 input_fda.spline_range = [0 24];
 input_fda.spline_nbasis_z = 7;
@@ -37,22 +39,26 @@ input_fda.spline_nbasis_sigma = 5;
 o_fda = stem_fda(input_fda);
 
 % object stem_data creation
+
 input_data.stem_modeltype = o_modeltype;
 input_data.data_table = data;
 input_data.stem_fda = o_fda;
 o_data = stem_data(input_data);
 
 % validation
-% S_val = randsample(51, round(0.3*51))'; 
+ 
 o_data.stem_validation = stem_validation('pickups', S_val);
 
 % object stem_par creation
+
 o_par = stem_par(o_data,'exponential');
 
 % object stem_model creation
+
 o_model = stem_model(o_data, o_par);
 
 % parameters initialization
+
 n_basis=o_fda.get_basis_number;
 o_par.beta = o_model.get_beta0();
 o_par.sigma_eps = o_model.get_coe_log_sigma_eps0();
@@ -61,26 +67,21 @@ o_par.G = diag(ones(n_basis.z,1)*0.5);
 o_par.v_z = eye(n_basis.z)*.5;
 o_model.set_initial_values(o_par);
 
-%% Profiles plotting
-
-lat0 = 40.7177; 
-lon0 = -74.0438;
-t_start = 1;
-t_end = 60;
-o_model.plot_profile(lat0, lon0, t_start, t_end);
-
 %% Model estimation
 
 % EM parameters
+
 o_EM_options = stem_EM_options();
 o_EM_options.exit_tol_par = 0.0001; 
 o_EM_options.exit_tol_loglike = 0.0001; 
 o_EM_options.max_iterations = 200;
 
 % EM estimation
+
 o_model.EM_estimate(o_EM_options);
 
 % Variance-covariance matrix evaluation
+
 delta=0.001;
 o_model.set_varcov(delta);
 o_model.set_logL();
@@ -89,11 +90,11 @@ o_model.plot_par
 %% Print and plots
 
 % model estimation results
+
 o_model.print
 o_model.beta_Chi2_test
-
-% Figure 3 of the paper
 o_model.plot_par
+o_model.plot_validation
 
 %% Model saving
 
